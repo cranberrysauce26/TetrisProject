17c2f53ae8e75e1572bcf933edc67320418d6434
Steven <stevenmai132@yahoo.ca>
2018-06-05 13:20:49 -0400

    works now

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Tetris Panel");
         frame.add(new TetrisPanel());
-        frame.setSize(800, 600);
+        frame.setSize(400, 800);
         frame.setLocation(10, 100);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
